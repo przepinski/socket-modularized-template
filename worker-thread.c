ccba10e71f8b01633fbfac81ee71d2c40a2eb9c8
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-04-24 19:10:11 +0200

    refactor worker thread, modularize

--- worker-thread.c
+++ worker-thread.c
@@ -1,19 +1,39 @@
 #include "common.h"
 #include "worker-thread.h"
 
+void setWorkerThreadSignalHandling(sigset_t *previousMask)
+{
+    sigset_t blockMask = prepareBlockMask();
+    if (pthread_sigmask(SIG_BLOCK, &blockMask, previousMask) < 0)
+        ERR("pthread_sigmask");
+}
+
+void workerThreadCleanup(workerThreadArgs_t *workerArgs, sigset_t *previousMask)
+{
+    safeRemoveWorkerThreadFromList(workerArgs->workerThreadsList, pthread_self(), workerArgs->workerThreadsListMutex);
+
+
+    free(workerArgs);
+    if (pthread_sigmask(SIG_SETMASK, previousMask, NULL) < 0)
+        ERR("pthread_sigmask");
+}
+
 void *workerThread(void *args)
 {
     workerThreadArgs_t *workerArgs = (workerThreadArgs_t*)args;
-    sigset_t blockMask = prepareBlockMask(),
-        previousMask;
-    if (pthread_sigmask(SIG_BLOCK, &blockMask, &previousMask) < 0)
-        ERR("pthread_sigmask");
+    sigset_t previousMask;
+    setWorkerThreadSignalHandling(&previousMask);
     printf("[%d] started\n", workerArgs->id);
 
+    clientNode_t *client = safePopClientFromQueue(workerArgs->clientQueue, workerArgs->clientQueueMutex);
 
-    printf("[%d] terminates\n", workerArgs->id);
-    free(args);
-    if (pthread_sigmask(SIG_SETMASK, &previousMask, NULL) < 0)
-        ERR("pthread_sigmask");
+    printf("[%d] closing connection to the client\n", workerArgs->id);
+    if (TEMP_FAILURE_RETRY(close(client->clientSocket)) < 0)
+        ERR("close");
+    free(client);
+
+    int threadId = workerArgs->id;
+    workerThreadCleanup(workerArgs, &previousMask);
+    printf("[%d] terminated\n", threadId);
     return NULL;
 }
--- worker-thread.h
+++ worker-thread.h
@@ -1,15 +1,16 @@
 #ifndef WORKER_THREAD_H_INCLUDED
 #define WORKER_THREAD_H_INCLUDED
 
-#include <semaphore.h>
 #include <pthread.h>
 #include "client-queue.h"
+#include "worker-list.h"
 
 typedef struct workerThreadArgs {
     int id;
     clientNode_t **clientQueue;
     pthread_mutex_t *clientQueueMutex;
-    sem_t *gamesPendingSem;
+    workerThreadNode_t **workerThreadsList;
+    pthread_mutex_t *workerThreadsListMutex;
 } workerThreadArgs_t;
 
 void *workerThread(void *args);
